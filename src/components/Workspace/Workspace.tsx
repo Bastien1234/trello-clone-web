7fcd428dd17a1b5231af80c002d213a822f37ef6
unknown <bastien.clementi@gmail.com>
2022-06-09 21:17:40 +0200

    improved a bit

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import './workspace.scss';
 
 // Import logos
-import bellIcon from './../../assets/svg/bell.png';
-import infoIcon from './../../assets/svg/info.png';
+import trelloLogo from './../../assets/svg/logo.svg';
+import tableaux from './../../assets/svg/tableaux.png';
+import settings from './../../assets/svg/settings.png';
+import accueil from './../../assets/svg/accueil.png';
+import help from './../../assets/svg/help.png';
+
+const testDB = ["Foot", "Hokey"]
+
+
+
 
 
 function Workspace() {
     return (
-        <div>
+        <div className='workspace-main-container'>
             <header className='workspace-header'>
                 <div className='workspace-header-div-left'>
-                    <button className='button-4'>logo 1</button>
-                    <button className='button-4'>logo 2</button>
+                    <img src={trelloLogo} alt='logo' style={{backgroundColor: "white", borderRadius: 5}}/>
                     <button className='button-4'>Espaces de travail &darr;</button>
                     <button className='button-4'>Récent &darr;</button>
                     <button className='button-4'>Favoris &darr;</button>
@@ -22,11 +29,71 @@ function Workspace() {
 
                 <div className='workspace-header-div-right'>
                     <input placeholder="Parcourir" />
-                    <img src={infoIcon} alt='info icon' className='workspace-info-icon'/>
-                    <img src={bellIcon} alt='bell icon'  className='workspace-bell-icon'/>
                     <p>BC</p>
                 </div>
             </header>
+
+            <div className='home-container-main'>
+                <div className='home-container'>
+                    <div className='home-container-left'>
+                        <div className='left-side-button'>
+                            <img src={tableaux} className='left-side-button-img'/>
+                            <p>Tableaux</p>
+                        </div>
+                        <div className='left-side-button'>
+                        <img src={accueil} className='left-side-button-img'/>
+                            <p>Acceuil</p>
+                        </div>
+                        <div className='left-side-button'>
+                        <img src={settings} className='left-side-button-img'/>
+                            <p>Paramètres</p>
+                        </div>
+                        <div className='left-side-button'>
+                        <img src={help} className='left-side-button-img'/>
+                            <p>Au secours !</p>
+                        </div>
+                    </div>
+
+                    <div className='home-container-right'>
+                       <h1>VOS ESPACES DE TRAVAIL</h1>
+
+                       <div style={{
+                           display: "flex",
+                           flexDirection: "row",
+
+                       }}>
+                           {
+                               testDB.map((el, idx) => {
+                                   return(
+                                       <div style={{
+                                           backgroundColor: "rgb(6, 95, 157",
+                                           borderRadius: 10,
+                                           width: 120,
+                                           height: 90,
+                                           marginRight: 15,
+                                           display: "flex",
+                                           justifyContent: "flex-start",
+                                           alignItems: "flex-start",
+                                           cursor: "pointer"
+                                       }}>
+                                           <p style={{
+                                               color: "white",
+                                               fontWeight: "bold",
+                                               marginLeft: 10
+                                           }}>{el}</p>
+                                       </div>
+                                   )
+                               })
+                           }
+
+                            <div className="create-button">
+                                <p>Créer un tableau</p>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
